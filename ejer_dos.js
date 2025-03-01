8ab80204f966139b1181839c8a64df8ae38e103f
CamiiUribeee <acuribel@ufpso.edu.co>
2025-03-01 13:03:05 -0500

    ejemplo real

--- ejer_dos.js
+++ ejer_dos.js
@@ -2,3 +2,5 @@
 function suma(a, b) {
     return a + b;
 }
+const resultado = suma(24, 5);
+console.log(`La suma es: ${resultado}`);
--- ejer_dos.ts
+++ ejer_dos.ts
@@ -1,3 +1,5 @@
 function suma (a:number, b:number): number {
     return a+b; 
 }
+const resultado = suma(24, 5);
+console.log(`La suma es: ${resultado}`);
